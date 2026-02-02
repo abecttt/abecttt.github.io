503281921e322716b1031e5f5d640ca8bf2d3a8c
abecttt <zurjea@gmail.com>
2026-02-01 21:05:00 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const images = [
+  "images/claire1.jpg",
+  "images/claire2.jpg",
+  "images/claire3.jpg"
+];
+
+let i = 0;
+const slide = document.getElementById("slide");
+
+setInterval(() => {
+  i = (i + 1) % images.length;
+  slide.style.opacity = 0;
+  setTimeout(() => {
+    slide.src = images[i];
+    slide.style.opacity = 1;
+  }, 400);
+}, 3000);
